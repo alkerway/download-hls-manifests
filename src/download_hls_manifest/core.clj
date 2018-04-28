65cf502f3ee984004ec6b4deded9d368b0c2214e
walkerflocker <1042.aw@gmail.com>
2018-04-28 16:05:02 -0600

    frag downloads working

--- src/download_hls_manifest/core.clj
+++ src/download_hls_manifest/core.clj
@@ -1,5 +1,6 @@
 (ns download-hls-manifest.core
   (:require [clj-http.client :as client]
+            [me.raynes.fs :as fs]
             [download-hls-manifest.parser :as parser]
             [download-hls-manifest.config :as config]
             [clojure.string :as cljstr]
@@ -10,14 +11,24 @@
     (with-open [wrtr (io/writer (str config/basePath "/" filePath))]
       (.write wrtr body))
     body))
+
+(defn getFrag [fragUrl levelUrl]
+  (let [filePath (parser/getLocalPath fragUrl levelUrl)
+        remotePath (parser/getRemotePath fragUrl levelUrl)]
+    (println filePath)
+    (println remotePath)
+    (fs/mkdir (str config/basePath "/" (cljstr/join "/" (pop (cljstr/split filePath #"/")))))
+    (saveRemoteToFile remotePath filePath)))
   
   
 (defn getFrags [levelUrl masterUrl]
   (let [filePath (parser/getLocalPath levelUrl masterUrl)
         remotePath (parser/getRemotePath levelUrl masterUrl)
         fragUrls (parser/getFragUrls (saveRemoteToFile remotePath filePath))]
-    (println (count fragUrls))
-    ))
+    (loop [frags fragUrls]
+      (let [eachFrag (first frags)]
+        (if eachFrag (do (getFrag eachFrag remotePath)
+                         (recur (rest frags))))))))
 
 
 (defn downloadMaster [masterUrl]
@@ -28,4 +39,4 @@
         (if eachUrl (do (getFrags eachUrl masterUrl)
           (recur (rest levels))))))
     {:status 200
-     :body master}))
+     :body "success"}))
